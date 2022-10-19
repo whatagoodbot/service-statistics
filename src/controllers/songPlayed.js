8320f69b9da34d2d4859432a9b6d148ba3bf14ea
notABluesSinger <rob@whatagoodbot.com>
2022-10-19 12:34:09 +0100

    setting message broker version

--- src/controllers/songPlayed.js
+++ src/controllers/songPlayed.js
@@ -1,6 +1,5 @@
 import { playsDb } from '../models/index.js'
 
 export default async payload => {
-  console.log('played', payload)
   playsDb.add(payload.dj.userId, payload.room, payload.artist, payload.title, payload.details.id, payload.details.provider, payload.meta?.theme?.id)
 }
